a17c1d819c4fb5836d0c35eb6ee22dec1eaf1973
Ernest Litvinenko <ernest.litvinenko@mail.ru>
2023-06-22 06:22:58 +1000

    upd add db connection

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+from datetime import datetime, timezone
+
 import uvicorn
 from fastapi import FastAPI
 from config import Config
+from loguru import logger
+from database import connect_database
 
 app = FastAPI()
+
+
+@app.on_event('startup')
+async def on_startup():
+    await connect_database()
+    logger.success('Application startup complete at {time}', time=datetime.now(tz=timezone.utc))
 
 
 if __name__ == '__main__':
