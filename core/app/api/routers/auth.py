8408076717d156f27a5500c0f719b33ef44316a8
Ernest Litvinenko <73121802+ernestlitvinenko@users.noreply.github.com>
2023-06-22 10:54:05 +0300

    Add todos for auth.py

--- core/app/api/routers/auth.py
+++ core/app/api/routers/auth.py
@@ -5,7 +5,7 @@ from app.core.auth.jwt import create_access_token, get_current_user
 router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/token")
 
-
+# TODO Поменять на multipart form data
 @router.post("/token")
 async def login(username: str, password: str):
     # Здесь должна быть ваша логика проверки учетных данных (например, валидация пользователя в базе данных)
@@ -18,7 +18,7 @@ async def login(username: str, password: str):
 
     return {"access_token": access_token, "token_type": "bearer"}
 
-
+# TODO DEPRECATED можно убрать или отставить как пример
 @router.get("/protected")
 async def protected_route(current_user: str = Depends(get_current_user)):
     # В этом эндпоинте вы можете разместить защищенную функциональность, к которой имеют доступ только аутентифицированные пользователи
